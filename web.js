69501e070f0efb0773ae2e9a8865e45658ab0e41
Jiri Hroza <jiri.hroza@gmail.com>
2013-07-10 14:43:15 +0000

    index.html loaded from disk

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var readIndex = function(filename)
+{
+    var fs = require('fs');
+
+    var x = fs.readFileSync(filename);
+
+    //console.log("[" + x.toString() + "]")
+    return x.toString();
+}
+
+app.get('/', function(request, response){
+    content = readIndex('index.html')
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
